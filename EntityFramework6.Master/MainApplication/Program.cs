73e893091f68c6229dd64803bc5c37c931dde182
jnarkiewicz <jdnark@gmail.com>
2016-09-20 18:51:00 -0700

    Basic Code

---
+++ EntityFramework6.Master/MainApplication/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using DataAccess;
+
+namespace MainApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SchoolEntities entities = new SchoolEntities())
+            {
+                foreach (UserType userType in entities.UserTypes)
+                {
+                }
+            }
+        }
+    }
+}
