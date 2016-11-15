1147058c2e106954708805778025532ed0cd4739
jnarkiewicz <jdnark@gmail.com>
2016-11-14 19:24:49 -0800

    Basic email send via Godaddy

---
+++ GoDaddySendEmailMaster/GoDaddySendEmail/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace GoDaddySendEmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (SmtpClient smtpClient = new SmtpClient())
+                {
+                    smtpClient.Send(
+                        "KeyserSoeze@galacticnachos.com",
+                        "VerbalKint@galacticnachos.com",
+                        "any subject",
+                        "any body");
+                    smtpClient.Dispose();
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+            }
+        }
+    }
+}
