906fc8f98b8a4fdbde0f641bda44c641346c5176
softwarepronto <jann@softwarepronto.com>
2017-11-06 02:04:14 -0800

    Uploaded SmtpClient appl.config/web.config sample

---
+++ MailSettingsMaster/MailSettingsApplication/Mail.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Mail; // add reference to System.Net
+
+namespace MailSettingsApplication
+{
+    public class Mail
+    {
+        public void Send(string recipient, string subject, string message)
+        {
+            SmtpClient client = new SmtpClient();
+            MailMessage mail = new MailMessage();
+
+            mail.To.Add(recipient);
+            mail.Subject = subject;
+            mail.Body = message;
+            client.Send(mail);
+        }
+    }
+}
---
+++ MailSettingsMaster/MailSettingsApplication/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSettingsApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string dateFormat = "yyyyMMdd hh:mm:ss.fff";
+                Mail mail = new Mail();
+
+                mail.Send(
+                    "jdnark@gmail.com",
+                    $"This is it {DateTime.UtcNow.ToString(dateFormat)}",
+                    "I guess it would be nice if I could touch  your body because not everybody's got a body like you.");
+            }
+
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+            }
+        }
+    }
+}
