a7cda0a5406020c57695d0c77e74de505b27d629
Jan David Narkiewicz <jann@softwarepronto.com>
2022-09-25 21:14:44 -0700

    Removed Twitter Volumne Stream (moved to other repo)

--- Twitter.VolumeStream/Twitter.VolumeStream/Interfaces/IReportSource.cs
+++
@@ -1,9 +0,0 @@
-﻿// Licensed to the softwarepronto.com blog under the GNU General Public License.
-
-namespace Twitter.VolumeStream.Interfaces
-{
-    public interface IReportSource
-    {
-        void Write(string output);
-    }
-}
--- Twitter.VolumeStream/Twitter.VolumeStream/Interfaces/ITweetReader.cs
+++
@@ -1,9 +0,0 @@
-﻿// Licensed to the softwarepronto.com blog under the GNU General Public License.
-
-namespace Twitter.VolumeStream.Interfaces
-{
-    public interface ITweetReader : IDisposable
-    {
-        Task<string?> ReadLineAsync();
-    }
-}
